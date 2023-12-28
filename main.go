9faf2289e8be860340bc656bb14d9bdf794469ea
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-12-28 14:43:23 +0100

    added handlers and routes

--- main.go
+++ main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/Svenpaj/test-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	log.Print("listening to - ", server.Addr)
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to lister to server", err)
+		fmt.Println("failed to start app:", err)
 	}
-
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
 }
