3a20b407e8535555d2b1e1301e67152eb14ba29e
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-12-28 15:12:24 +0100

    added handlers for more routes

--- application/routes.go
+++ application/routes.go
@@ -3,6 +3,7 @@ package application
 import (
 	"net/http"
 
+	"github.com/Svenpaj/test-api/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -16,9 +17,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
 }
 
 func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
 
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetById)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
